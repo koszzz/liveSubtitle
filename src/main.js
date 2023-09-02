f962145432bd343699016c93859dcdf2eff3d5ad
koszzz <huangguacucumber@outlook.com>
2023-09-02 15:17:31 +0800

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
